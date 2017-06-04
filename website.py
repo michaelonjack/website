584d4a518a966d7c47e5565509413c381f322202
Michael Onjack <mikeonjack@gmail.com>
2017-06-04 18:55:14 -0400

    Adds accordion and carousel to sample page

--- website.py
+++ website.py
@@ -9,9 +9,6 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 
 
 
-PASSWORD = "muffy"
-
-
 # THREE FUNCTIONS FOR RENDERING BASIC TEMPLATES
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -61,39 +58,6 @@ class ContactPage(Handler):
 class SamplesPage(Handler):
     def get(self):
         self.render("samples_website.html")
-
-class RoommatesPage(Handler):
-    def get(self):
-	self.render("roommates_website.html")
-	
-class ElectricPage(Handler):
-    def get(self):
-        self.render("electric_website.html")
-        
-class PreviousElectricPage(Handler):
-	def get(self):
-		self.render("previous_electric_website.html");
-        
-class MuffinPage(Handler):
-    def get(self):
-        self.render("muffin_website.html")
-        
-class PasswordPage(Handler):
-    def write_form(self, error=""):
-        self.render("password.html", error=error)
-    
-    
-    def post(self):
-    	password_given = self.request.get("password")
-    	
-    	if PASSWORD == password_given:
-    		self.redirect("/roommates/justmuffinstuff")
-    	else:
-    		self.write_form("INCORRECT PASSWORD")
-    
-    
-    def get(self):
-        self.render("password.html")
 		
 
 
@@ -101,11 +65,6 @@ application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/info', InfoPage),
     ('/contact', ContactPage),
-    ('/samples',SamplesPage),
-    ('/roommates',RoommatesPage),
-    ('/roommates/electric', ElectricPage),
-    ('/roommates/electric/previous', PreviousElectricPage),
-    ('/roommates/justmuffinstuff', MuffinPage),
-    ('/roommates/password', PasswordPage)
-    ], debug=True)
+    ('/samples',SamplesPage)
+], debug=True)
     
