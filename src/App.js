285766ec90e264578c7f9c402c3f342797a07904
Michael Onjack <mikeonjack@gmail.com>
2021-02-14 17:19:56 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
+  console.log('here')
+  console.log(process.env.REACT_APP_ENV)
   if(process.env.REACT_APP_ENV !== 'development'
     && window.location.protocol !== 'https:') {
     const protocol = window.location.protocol
